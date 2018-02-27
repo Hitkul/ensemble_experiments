148259daaa15f283ee469b796cdb2a7075a97108
Hitkul <hitkuljangid@gmail.com>
2018-02-27 19:12:58 +0530

    script version

--- MR_CNN.py
+++ MR_CNN.py
@@ -1,18 +1,10 @@
 
-# coding: utf-8
-
-# In[184]:
-
-
 import json
 import string
 from nltk.corpus import stopwords
 import numpy as np
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
-import matplotlib.pyplot as plt
-get_ipython().magic('matplotlib inline')
-plt.rcParams["figure.figsize"] = [12,9]
 from gensim.models import KeyedVectors
 import word2vecReader as godin_embedding
 import fasttext
@@ -28,59 +20,33 @@ from random import uniform,choice
 from os import remove
 
 
-# In[185]:
-
-
 def load_data(filename):
     with open(filename,'r') as fin:
         return json.load(fin)
 
 
-# In[186]:
-
-
 dataset_dict = load_data("dataset/final_dataset.json")
-
-
-# In[187]:
-
 
 def extract_data(dataset_dict):
     review = [dataset_dict[key]["review"] for key in dataset_dict.keys()]
     polarity = [dataset_dict[key]["polarity"] for key in dataset_dict.keys()]
     return review, polarity 
 
-
-# In[188]:
-
-
 review, polarity = extract_data(dataset_dict)
 
 
-# In[218]:
-
-
-# review = review[:100]
-# polarity = polarity[:100]
-
-
-# In[190]:
-
-
-len(review),len(polarity)
-
-
-# In[191]:
-
+def remove_punctuation(s):
+    list_punctuation = list(string.punctuation)
+    for i in list_punctuation:
+        s = s.replace(i,'')
+    return s
 
 def clean_sentence(sentence):
-    # split into tokens by white space
     tokens = sentence.split()
-    # remove punctuation from each token
-    table = str.maketrans('', '', string.punctuation)
-    tokens = [w.translate(table) for w in tokens]
+    # table = str.maketrans('', '', string.punctuation)
+    # tokens = [w.translate(table) for w in tokens]
     #for PYTHON 2.7
-    #tokens = [w.translate(None, string.punctuation) for w in tokens]
+    tokens = [remove_punctuation(w) for w in tokens]
     # remove remaining tokens that are not alphabetic
     tokens = [word for word in tokens if word.isalpha()]
     # filter out stop words
@@ -91,41 +57,20 @@ def clean_sentence(sentence):
     tokens = ' '.join(tokens)
     return tokens
 
-
-# In[192]:
-
-
+    
+print "cleaning reviews..."
 review = [clean_sentence(s) for s in review]
-
-
-# In[193]:
 
 
 length = [len(s) for s in review]
 
-
-# In[194]:
-
-
-plt.hist(length)
-
-
-# In[195]:
-
-
 max_len = 6500
-
-
-# In[196]:
-
 
 def create_tokenizer(lines):
     tokenizer = Tokenizer()
     tokenizer.fit_on_texts(lines)
     return tokenizer
 
-
-# In[197]:
 
 
 def encode_text(tokenizer, lines, length):
@@ -134,12 +79,10 @@ def encode_text(tokenizer, lines, length):
     return padded
 
 
-# In[198]:
-
 
 #loading GloVe embedding
 def load_GloVe_embedding(file_name):
-    print('Loading GloVe word vectors.')
+    print 'Loading GloVe word vectors.'
     embeddings_index = dict()
     f = open(file_name)
     for line in f:
@@ -149,9 +92,6 @@ def load_GloVe_embedding(file_name):
         embeddings_index[word] = coefs
     f.close()
     return embeddings_index
-
-
-# In[199]:
 
 
 # create a weight matrix for words in training docs
@@ -164,28 +104,22 @@ def get_GloVe_embedding_matrix(embeddings_index):
     return embedding_matrix
 
 
-# In[200]:
-
 
 #fast text word embedding
 def load_fast_text_model(sentences):
     try:
         m = fasttext.load_model('word_embeddings/fast_text_model.bin')
-        print("trained model loaded")
+        print "trained model loaded"
         return m
     except:
-        print("traning new model")
+        print "traning new model"
         with open('temp_file.txt','w') as temp_file:
             for sentence in sentences:
                 temp_file.write(sentence)
         m = fasttext.cbow('temp_file.txt','word_embeddings/fast_text_model')
         remove('temp_file.txt')
-        print('model trained')
+        print 'model trained'
         return m
-
-
-# In[201]:
-
 
 def get_fast_text_matrix(model):
     embedding_matrix = np.zeros((vocab_size,100))
@@ -199,16 +133,12 @@ def get_fast_text_matrix(model):
     return embedding_matrix
 
 
-# In[202]:
-
 
 #loading godin word embedding
 def load_godin_word_embedding(path):
-    print("Loading Goding model.")
+    print "Loading Goding model."
     return godin_embedding.Word2Vec.load_word2vec_format(path, binary=True)
 
-
-# In[203]:
 
 
 def get_godin_embedding_matrix(model):
@@ -223,16 +153,12 @@ def get_godin_embedding_matrix(model):
     return embedding_matrix
 
 
-# In[204]:
-
 
 #loading Google Word2Vec
 def load_google_word2vec(file_name):
-    print("Loading google news word2vec")
+    print "Loading google news word2vec"
     return KeyedVectors.load_word2vec_format(file_name, binary=True)
 
-
-# In[205]:
 
 
 def get_word2vec_embedding_matrix(model):
@@ -246,8 +172,6 @@ def get_word2vec_embedding_matrix(model):
             embedding_matrix[i]=embedding_vector
     return embedding_matrix
 
-
-# In[206]:
 
 
 def define_model(length,vocab_size,n_dense,dropout,learning_rate,n_filters,filter_size_c1,filter_size_c2,filter_size_c3,em_c1,em_c2,em_c3,free_em_dim,em_trainable_flag_c1,em_trainable_flag_c2,em_trainable_flag_c3):
@@ -308,43 +232,26 @@ Y = np.asarray(polarity)
 
 tokenizer = create_tokenizer(X)
 vocab_size = len(tokenizer.word_index) + 1
-print('Max document length: %d' % max_len)
-print('Vocabulary size: %d' % vocab_size)
 X = encode_text(tokenizer, X, max_len)
 
 
-# In[176]:
-
-
 glove_model = load_GloVe_embedding('word_embeddings/glove.6B.300d.txt')
-# fast_text_model = load_fast_text_model(review)
-# godin_model = load_godin_word_embedding("word_embeddings/word2vec_twitter_model.bin")
-# word2vec_model= load_google_word2vec('word_embeddings/GoogleNews-vectors-negative300.bin')
-
-
-# In[177]:
+fast_text_model = load_fast_text_model(review)
+godin_model = load_godin_word_embedding("word_embeddings/word2vec_twitter_model.bin")
+word2vec_model= load_google_word2vec('word_embeddings/GoogleNews-vectors-negative300.bin')
 
 
 embedding_matrix_glove = get_GloVe_embedding_matrix(glove_model)
-# embedding_matrix_word2vec = get_word2vec_embedding_matrix(word2vec_model)
-# embedding_matrix_fast_text = get_fast_text_matrix(fast_text_model)
-# embedding_matrix_godin = get_godin_embedding_matrix(godin_model)
-
-
-# In[209]:
+embedding_matrix_word2vec = get_word2vec_embedding_matrix(word2vec_model)
+embedding_matrix_fast_text = get_fast_text_matrix(fast_text_model)
+embedding_matrix_godin = get_godin_embedding_matrix(godin_model)
 
 
 seed = 7
 np.random.seed(seed)
 
 
-# In[210]:
-
-
 kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=seed)
-
-
-# In[211]:
 
 
 def get_parameters():
@@ -352,14 +259,12 @@ def get_parameters():
     para_n_dense = [100,200,300,400]
     para_n_filters = [100,200,300,400]
     para_filter_size = [1,2,3,4,5,6]
-#     para_em = ['embedding_matrix_fast_text','embedding_matrix_godin','embedding_matrix_word2vec','embedding_matrix_glove','free']
-    para_em = ['embedding_matrix_glove','free']
+    para_em = ['embedding_matrix_fast_text','embedding_matrix_godin','embedding_matrix_word2vec','embedding_matrix_glove','free']
     para_free_em_dim = [100,300,400]
     para_em_trainable_flag = [True,False]
     para_batch_size = [8,16,32,64]
-    para_epoc = [10,30,60,100]
-#     para_epoc = [1]
-#     para_batch_size = [8]
+    # para_epoc = [10,30,60,100]
+    para_epoc = [30]
     #selecting_random_value
     parameters = {"n_dense": choice(para_n_dense),
             "dropout": uniform(0.4, 0.9),
@@ -381,20 +286,10 @@ def get_parameters():
     return parameters
 
 
-# In[215]:
-
-
 key = 1
 record = {}
 
-
-# In[216]:
-
-
 number_of_models = 2
-
-
-# In[217]:
 
 
 for _ in range(number_of_models):
@@ -439,3 +334,4 @@ for _ in range(number_of_models):
         json.dump(record,fout,indent=4)
     key+=1
 
+print "model done"
