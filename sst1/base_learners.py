63a884c763ed083c7e2bdf41d200caa9db3c6e80
Hitkul <hitkuljangid@gmail.com>
2018-03-08 12:38:00 +0530

    cnn done

--- sst1/base_learners.py
+++ sst1/base_learners.py
@@ -5,24 +5,13 @@ from keras.layers import Conv1D, GlobalMaxPooling1D
 from keras.optimizers import Adam
 
 
-# set parameters:
-
-batch_size = 32
-filters = 250
-kernel_size = 3
-hidden_dims = 250
-epochs = 2
-
-
-
-def cnn(vocab_size,n_dense,dropout,learning_rate,n_filters,filter_size,em,free_em_dim,number_of_classes):
+def cnn(length,vocab_size,n_dense,dropout,learning_rate,n_filters,filter_size,em,free_em_dim,number_of_classes,em_trainable_flag):
     model = Sequential()
     if em == 'free':
-        model.add(Embedding(vocab_size, free_em_dim,trainable = True))
+        model.add(Embedding(vocab_size,free_em_dim,trainable = True))
     else:
-        model.add(Embedding(Embedding(vocab_size, len(eval(em)[0]), weights = [eval(em)],input_length=length,trainable = True))
+        model.add(Embedding(vocab_size, len(em[0]), weights = [em],input_length=length,trainable = em_trainable_flag))
     model.add(Dropout(dropout))
-
     model.add(Conv1D(filters=n_filters, kernel_size=filter_size, activation='relu'))
     # we use max pooling:
     model.add(GlobalMaxPooling1D())
@@ -33,9 +22,14 @@ def cnn(vocab_size,n_dense,dropout,learning_rate,n_filters,filter_size,em,free_e
     model.add(Activation('relu'))
 
     # We project onto a single unit output layer, and squash it with a sigmoid:
-    model.add(Dense(number_of_classes))
-    model.add(Activation('sigmoid'))
+    if number_of_classes == 2:
+        model.add(Dense(1))
+        model.add(Activation('sigmoid'))
+    else:
+        model.add(Dense(number_of_classes))
+        model.add(Activation('softmax'))
 
     optimizer = Adam(lr=learning_rate)
     model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+    print(model.summary())
     return model
