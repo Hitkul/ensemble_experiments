50c29a6dbaf2d31c5f31834e870168acb3ade849
Hitkul <hitkuljangid@gmail.com>
2018-03-08 14:25:43 +0530

    cc-bi-lstm/gru added

--- sst1/base_learners.py
+++ sst1/base_learners.py
@@ -1,7 +1,7 @@
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation
 from keras.layers import Embedding
-from keras.layers import Conv1D, GlobalMaxPooling1D
+from keras.layers import Conv1D, GlobalMaxPooling1D, MaxPooling1D
 from keras.layers import LSTM,GRU,Bidirectional
 from keras.optimizers import Adam
 
@@ -88,6 +88,41 @@ def bi_gru(length,vocab_size,learning_rate,dropout,units_out,em,number_of_classe
     else:
         model.add(Dense(number_of_classes, activation='softmax'))
     
+    optimizer = Adam(lr=learning_rate)
+    model.compile(loss='binary_crossentropy',optimizer=optimizer,metrics=['accuracy'])
+    print(model.summary())
+    return model
+
+
+def cnn_bi_lstm(length,vocab_size,n_filters,filter_size,em,number_of_classes,em_trainable_flag,learning_rate,conv_dropout,l_or_g_dropout,units_out):
+    model = Sequential()
+    model.add(Embedding(vocab_size, len(em[0]), weights = [em],input_length=length,trainable = em_trainable_flag))
+    model.add(Conv1D(filters=n_filters, kernel_size=filter_size, activation='relu'))
+    model.add(Dropout(conv_dropout))
+    model.add(MaxPooling1D(pool_size=2))
+    model.add(Bidirectional(LSTM(units_out)))
+    model.add(Dropout(l_or_g_dropout))
+    if number_of_classes == 2:
+        model.add(Dense(1, activation='sigmoid'))
+    else:
+        model.add(Dense(number_of_classes, activation='softmax'))
+    optimizer = Adam(lr=learning_rate)
+    model.compile(loss='binary_crossentropy',optimizer=optimizer,metrics=['accuracy'])
+    print(model.summary())
+    return model
+
+def cnn_bi_gru(length,vocab_size,n_filters,filter_size,em,number_of_classes,em_trainable_flag,learning_rate,conv_dropout,l_or_g_dropout,units_out):
+    model = Sequential()
+    model.add(Embedding(vocab_size, len(em[0]), weights = [em],input_length=length,trainable = em_trainable_flag))
+    model.add(Conv1D(filters=n_filters, kernel_size=filter_size, activation='relu'))
+    model.add(Dropout(conv_dropout))
+    model.add(MaxPooling1D(pool_size=2))
+    model.add(Bidirectional(GRU(units_out)))
+    model.add(Dropout(l_or_g_dropout))
+    if number_of_classes == 2:
+        model.add(Dense(1, activation='sigmoid'))
+    else:
+        model.add(Dense(number_of_classes, activation='softmax'))
     optimizer = Adam(lr=learning_rate)
     model.compile(loss='binary_crossentropy',optimizer=optimizer,metrics=['accuracy'])
     print(model.summary())
