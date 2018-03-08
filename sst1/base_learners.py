993f3b1b58d5fa8d95b0a24fa9fc220a4f0548dc
Hitkul <hitkuljangid@gmail.com>
2018-03-08 13:27:11 +0530

    adding gru

--- sst1/base_learners.py
+++ sst1/base_learners.py
@@ -2,7 +2,7 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation
 from keras.layers import Embedding
 from keras.layers import Conv1D, GlobalMaxPooling1D
-from keras.layers import LSTM
+from keras.layers import LSTM,GRU
 from keras.optimizers import Adam
 
 
@@ -35,10 +35,24 @@ def cnn(length,vocab_size,n_dense,dropout,learning_rate,n_filters,filter_size,em
     return model
 
 
-def lstm(length,vocab_size,learning_rate,dropout,lstm_out,em,number_of_classes,em_trainable_flag):
+def lstm(length,vocab_size,learning_rate,dropout,units_out,em,number_of_classes,em_trainable_flag):
     model = Sequential()
     model.add(Embedding(vocab_size, len(em[0]), weights = [em],input_length=length,trainable = em_trainable_flag))
-    model.add(LSTM(lstm_out, dropout=dropout, recurrent_dropout=dropout))
+    model.add(LSTM(units_out, dropout=dropout, recurrent_dropout=dropout))
+    if number_of_classes == 2:
+        model.add(Dense(1, activation='sigmoid'))
+    else:
+        model.add(Dense(number_of_classes, activation='softmax'))
+    
+    optimizer = Adam(lr=learning_rate)
+    model.compile(loss='binary_crossentropy',optimizer=optimizer,metrics=['accuracy'])
+    print(model.summary())
+    return model
+
+def gru(length,vocab_size,learning_rate,dropout,units_out,em,number_of_classes,em_trainable_flag):
+    model = Sequential()
+    model.add(Embedding(vocab_size, len(em[0]), weights = [em],input_length=length,trainable = em_trainable_flag))
+    model.add(GRU(units_out, dropout=dropout, recurrent_dropout=dropout))
     if number_of_classes == 2:
         model.add(Dense(1, activation='sigmoid'))
     else:
