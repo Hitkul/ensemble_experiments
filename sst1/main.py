7868a37cd395a60eedadee6e4045c70c7748ed4f
Hitkul <hitkuljangid@gmail.com>
2018-03-21 18:41:13 +0530

    traning cnn

--- sst1/main.py
+++ sst1/main.py
@@ -1,5 +1,3 @@
-from __future__ import print_function
-
 import sys
 sys.path.append('../')
 import json
@@ -10,7 +8,6 @@ import pandas as pd
 from os import remove
 from pprint import pprint
 import re
-import codecs
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.utils import to_categorical
@@ -24,21 +21,15 @@ import skopt
 from skopt import gp_minimize, forest_minimize
 from skopt.space import Real, Categorical, Integer
 from skopt.utils import use_named_args
-from base_learners import cnn,lstm,gru,bi_lstm,bi_gru,cnn_bi_gru,cnn_bi_lstm,cnn_gru,cnn_lstm
-
-
-# In[2]:
+from base_learners import cnn,lstm,bi_lstm,cnn_bi_lstm,cnn_lstm
 
 
 def load_data_from_file(filename):
-    with codecs.open(filename,'r', errors='ignore') as fin:
+    with open(filename,'r', errors='ignore') as fin:
         lines = fin.readlines()
     label = [int(x.split()[0]) for x in lines]
     sentence = [' '.join(x.split()[1:]) for x in lines]
     return label,sentence
-
-
-# In[3]:
 
 
 train_labels,train_sentences = load_data_from_file('dataset/sst1/stsa.fine.train')
@@ -50,11 +41,34 @@ train_sentences = train_sentences+dev_sentence
 train_labels = train_labels+dev_label
 
 
+# In[5]:
+
+
+# len(train_labels),len(train_sentences),len(test_labels),len(test_sentences)
+
+
 # In[6]:
 
 
 number_of_classes = len(set(train_labels))
+# number_of_classes
 
+
+# In[7]:
+
+
+# for x in range(number_of_classes):
+#     print(x,train_labels.count(x))
+
+
+# # In[8]:
+
+
+# for x in range(number_of_classes):
+#     print(x,test_labels.count(x))
+
+
+# In[9]:
 
 
 def remove_punctuation(s):
@@ -65,6 +79,8 @@ def remove_punctuation(s):
 
 
 # In[10]:
+
+
 def clean_sentence(sentence):
     #removes links
     sentence = re.sub(r'(?P<url>https?://[^\s]+)', r'', sentence)
@@ -88,16 +104,29 @@ def clean_sentence(sentence):
     return tokens
 
 
+# In[11]:
+
+
 print("cleaning data")
 trainX = [clean_sentence(s) for s in train_sentences]
 testX = [clean_sentence(s) for s in test_sentences]
 trainY = np.array(train_labels)
 
 
-back_up_for_fasttext = trainX
-
-
 # In[13]:
+
+
+# lengths = [len(line.split()) for line in trainX]
+
+
+# In[14]:
+
+
+# print(max(lengths))
+# plt.hist(lengths)
+
+
+# In[15]:
 
 
 max_len = 24
@@ -121,54 +150,7 @@ def encode_text(tokenizer, lines, length):
     return padded
 
 
-# In[18]:
-
-
-def load_GloVe_embedding(file_name):
-    print('Loading GloVe word vectors.')
-    embeddings_index = dict()
-    f = open(file_name)
-    for line in f:
-        values = line.split()
-        word = values[0]
-        coefs = np.asarray(values[1:], dtype='float32')
-        embeddings_index[word] = coefs
-    f.close()
-    return embeddings_index
-
-
-# In[19]:
-
-
-def get_GloVe_embedding_matrix(embeddings_index):
-    embedding_matrix = np.zeros((vocab_size, 300))
-    for word, i in tokenizer.word_index.items():
-        embedding_vector = embeddings_index.get(word)
-        if embedding_vector is not None:
-            embedding_matrix[i] = embedding_vector
-    return embedding_matrix
-
-
 # In[20]:
-
-
-def load_fast_text_model(sentences):
-    try:
-        m = fasttext.load_model('fast_text_model.bin')
-        print("trained model loaded")
-        return m
-    except:
-        print("traning new model")
-        with open('temp_file.txt','w') as temp_file:
-            for sentence in sentences:
-                temp_file.write(sentence)
-        m = fasttext.cbow('temp_file.txt','fast_text_model')
-        remove('temp_file.txt')
-        print('model trained')
-        return m
-
-
-# In[21]:
 
 
 def load_godin_word_embedding(path):
@@ -176,7 +158,7 @@ def load_godin_word_embedding(path):
     return godin_embedding.Word2Vec.load_word2vec_format(path, binary=True)
 
 
-# In[22]:
+# In[21]:
 
 
 def load_google_word2vec(file_name):
@@ -184,7 +166,7 @@ def load_google_word2vec(file_name):
     return KeyedVectors.load_word2vec_format(file_name, binary=True)
 
 
-# In[23]:
+# In[22]:
 
 
 def get_word_embedding_matrix(model,dim):
@@ -202,7 +184,7 @@ def get_word_embedding_matrix(model,dim):
     return embedding_matrix
 
 
-# In[24]:
+# In[23]:
 
 
 tokenizer = create_tokenizer(trainX)
@@ -214,44 +196,45 @@ testX = encode_text(tokenizer, testX, max_len)
 trainY = to_categorical(trainY,num_classes=number_of_classes)
 
 
+# In[24]:
+
+
+# glove_model = load_GloVe_embedding('word_embeddings/glove.6B.300d.txt')
+# fast_text_model = load_fast_text_model(back_up_for_fasttext)
+# godin_model = load_godin_word_embedding("word_embeddings/word2vec_twitter_model.bin")
+word2vec_model= load_google_word2vec('../word_embeddings/GoogleNews-vectors-negative300.bin')
+
+
 # In[25]:
 
 
-glove_model = load_GloVe_embedding('../word_embeddings/glove.6B.300d.txt')
-fast_text_model = load_fast_text_model(back_up_for_fasttext)
-godin_model = load_godin_word_embedding("../word_embeddings/word2vec_twitter_model.bin")
-word2vec_model= load_google_word2vec('../word_embeddings/GoogleNews-vectors-negative300.bin')
+# embedding_matrix_glove = get_GloVe_embedding_matrix(glove_model)
+embedding_matrix_word2vec = get_word_embedding_matrix(word2vec_model,300)
+# embedding_matrix_fast_text = get_word_embedding_matrix(fast_text_model,100)
+# embedding_matrix_godin = get_word_embedding_matrix(godin_model,400)
 
 
 # In[26]:
 
 
-embedding_matrix_glove = get_GloVe_embedding_matrix(glove_model)
-embedding_matrix_word2vec = get_word_embedding_matrix(word2vec_model,300)
-embedding_matrix_fast_text = get_word_embedding_matrix(fast_text_model,100)
-embedding_matrix_godin = get_word_embedding_matrix(godin_model,400)
-
-
-# In[29]:
-
-
-para_learning_rate = Real(low=1e-4, high=1e-2, prior='log-uniform',name='learning_rate')
+para_learning_rate = Categorical(categories=[0.1,0.01,0.001,0.0001],name='learning_rate')
 para_dropout = Categorical(categories=[0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9],name = 'dropout')
-para_em = Categorical(categories=['embedding_matrix_fast_text','embedding_matrix_godin','embedding_matrix_word2vec','embedding_matrix_glove'],name='em')
+# para_em = Categorical(categories=[embedding_matrix_godin,embedding_matrix_word2vec],name='em')
+para_em = Categorical(categories=['embedding_matrix_word2vec'],name='em')
 para_em_trainable_flag = Categorical(categories=[True,False],name='em_trainable_flag')
 para_batch_size = Categorical(categories=[8,16,32,64],name='batch_size')
-para_epoch = Categorical(categories=[5,10,20,50,100],name='epoch')
+para_epoch = Categorical(categories=[10,15,20,50],name='epoch')
 
 # para_units_out = Categorical(categories=[64,128,256,512], name='units_out')
 
 # para_dropout_cnn_lstm = Categorical(categories=[0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9],name = 'dropout')
 
 para_n_dense = Categorical(categories=[100,200,300,400], name='n_dense')
-para_n_filters = Categorical(categories=[32,100,200,300],name='n_filters')
-para_filter_size = Integer(low=1,high=8,name = 'filter_size')
+para_n_filters = Categorical(categories=[100,200,300],name='n_filters')
+para_filter_size = Integer(low=1,high=6,name = 'filter_size')
 
 
-# In[30]:
+# In[27]:
 
 
 parameters_cnn = [para_learning_rate,para_dropout,para_n_dense,para_n_filters,para_filter_size,para_em,para_em_trainable_flag,para_batch_size,para_epoch]
@@ -259,22 +242,22 @@ parameters_cnn = [para_learning_rate,para_dropout,para_n_dense,para_n_filters,pa
 # parameters_cnn_lstm = [para_learning_rate,para_dropout,para_dropout_cnn_lstm,para_units_out,para_n_filters,para_filter_size,para_em,para_em_trainable_flag,para_batch_size,para_epoch]
 
 
-# In[33]:
+# In[32]:
 
 
-default_parameters_cnn = [0.001,0.2,300,100,5,'embedding_matrix_word2vec',True,32,5]
+default_parameters_cnn = [0.0001,0.6,100,200,1,'embedding_matrix_word2vec',True,8,20]
 # default_parameters_lstm = [0.001,0.2,128,embedding_matrix_word2vec,True,32,20]
 # default_parameters_cnn_lstm = [0.001,0.2,0.2,128,100,5,embedding_matrix_word2vec,True,32,10]
 
 
-# In[34]:
+# In[33]:
 
 
 key = 1
 record = {}
 
 
-# In[38]:
+# In[34]:
 
 
 ##this will change based on the model
@@ -289,7 +272,7 @@ def fitness(learning_rate,dropout,n_dense,n_filters,filter_size,em,em_trainable_
             "dropout": dropout,
             "n_dense": n_dense,
             "n_filters": n_filters,
-            "filter_size": filter_size,
+            "filter_size": int(filter_size),
             "em": em,
             "em_trainable_flag":em_trainable_flag,
             "batch": batch_size,
@@ -302,9 +285,9 @@ def fitness(learning_rate,dropout,n_dense,n_filters,filter_size,em,em_trainable_
                 vocab_size=vocab_size,
                 n_dense=parameters['n_dense'],
                 dropout=parameters['dropout'],
-                learning_rate=parameters['learning_rate'],
+                learning_rate = parameters['learning_rate'],
                 n_filters=parameters['n_filters'],
-                filter_size=int(parameters['filter_size']),
+                filter_size=parameters['filter_size'],
                 em = eval(parameters['em']),
                 number_of_classes=number_of_classes,
                 em_trainable_flag=parameters['em_trainable_flag'])
@@ -327,7 +310,7 @@ def fitness(learning_rate,dropout,n_dense,n_filters,filter_size,em,em_trainable_
     return -acc
 
 
-# In[39]:
+# In[35]:
 
 
 search_result = gp_minimize(func=fitness,
@@ -403,14 +386,14 @@ search_result = gp_minimize(func=fitness,
 # In[122]:
 
 
-# model = lstm(length=max_len,
+# model = bi_gru(length=max_len,
 #              vocab_size=vocab_size,
-#              learning_rate=parameters['learning_rate'],
-#              dropout=parameters['dropout'],
-#              units_out=parameters['units_out'],
-#              em=parameters['em'],
+#              learning_rate=parameters_lstm_or_gru['learning_rate'],
+#              dropout=parameters_lstm_or_gru['dropout'],
+#              units_out=parameters_lstm_or_gru['units_out'],
+#              em=parameters_lstm_or_gru['em'],
 #              number_of_classes=number_of_classes,
-#              em_trainable_flag=parameters['em_trainable_flag'])
+#              em_trainable_flag=parameters_lstm_or_gru['em_trainable_flag'])
 
 
 # In[123]:
