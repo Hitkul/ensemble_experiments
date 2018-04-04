690967e91fee6142e192af9a21121753c0889c04
Hitkul <hitkuljangid@gmail.com>
2018-04-04 17:23:46 +0530

    script version

---
+++ sst1/ensemble.py
@@ -0,0 +1,520 @@
+
+# coding: utf-8
+
+# In[236]:
+
+
+import matplotlib
+matplotlib.use('Agg')
+import sys
+sys.path.append('../')
+import json
+import string
+from nltk.corpus import stopwords
+import numpy as np
+import pandas as pd
+from os import remove
+from pprint import pprint
+import re
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils import to_categorical
+import keras.backend as K
+from gensim.models import KeyedVectors
+import word2vecReader as godin_embedding
+import fasttext
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import accuracy_score
+from base_learners import cnn,lstm,bi_lstm,cnn_bi_lstm,cnn_lstm
+# %matplotlib inline
+import matplotlib.pyplot as plt
+# plt.rcParams["figure.figsize"] = [12,10]
+from mlens.visualization import corrmat
+
+
+# In[176]:
+
+
+def load_data_from_file(filename):
+    with open(filename,'r', errors='ignore') as fin:
+        lines = fin.readlines()
+    label = [int(x.split()[0]) for x in lines]
+    sentence = [' '.join(x.split()[1:]) for x in lines]
+    return label,sentence
+
+
+# In[177]:
+
+
+train_labels,train_sentences = load_data_from_file('dataset/sst1/stsa.fine.train')
+dev_label,dev_sentence = load_data_from_file('dataset/sst1/stsa.fine.dev')
+test_labels,test_sentences = load_data_from_file('dataset/sst1/stsa.fine.test')
+
+
+# In[178]:
+
+
+train_sentences = train_sentences+dev_sentence
+train_labels = train_labels+dev_label
+
+
+# In[179]:
+
+
+len(train_labels),len(train_sentences),len(test_labels),len(test_sentences)
+
+
+# In[180]:
+
+
+train_labels = train_labels[:500]
+train_sentences = train_sentences[:500]
+test_labels=test_labels[:100]
+test_sentences = test_sentences[:100]
+
+
+# In[181]:
+
+
+number_of_classes = len(set(train_labels))
+number_of_classes
+
+
+# In[182]:
+
+
+len(train_labels),len(train_sentences),len(test_labels),len(test_sentences)
+
+
+# In[183]:
+
+
+def remove_punctuation(s):
+    list_punctuation = list(string.punctuation)
+    for i in list_punctuation:
+        s = s.replace(i,'')
+    return s
+
+
+# In[184]:
+
+
+def clean_sentence(sentence):
+    #removes links
+    sentence = re.sub(r'(?P<url>https?://[^\s]+)', r'', sentence)
+    # remove @usernames
+    sentence = re.sub(r"\@(\w+)", "", sentence)
+    #remove # from #tags
+    sentence = sentence.replace('#','')
+    # split into tokens by white space
+    tokens = sentence.split()
+    # remove punctuation from each token
+    # should have used translate but for some reason it breaks on my server
+    tokens = [remove_punctuation(w) for w in tokens]
+    # remove remaining tokens that are not alphabetic
+    tokens = [word for word in tokens if word.isalpha()]
+    # filter out stop words
+    stop_words = set(stopwords.words('english'))
+    tokens = [w for w in tokens if not w in stop_words]
+    # filter out short tokens
+    tokens = [word for word in tokens if len(word) > 1]
+    tokens = ' '.join(tokens)
+    return tokens
+
+
+# In[185]:
+
+
+print("cleaning data")
+trainX = [clean_sentence(s) for s in train_sentences]
+testX = [clean_sentence(s) for s in test_sentences]
+trainY = np.array(train_labels)
+testY=test_labels
+
+
+# In[186]:
+
+
+max_len = 24
+
+
+# In[187]:
+
+
+def create_tokenizer(lines):
+    tokenizer = Tokenizer()
+    tokenizer.fit_on_texts(lines)
+    return tokenizer
+
+
+# In[188]:
+
+
+def encode_text(tokenizer, lines, length):
+    encoded = tokenizer.texts_to_sequences(lines)
+    padded = pad_sequences(encoded, maxlen=length, padding='post')
+    return padded
+
+
+# In[189]:
+
+
+def load_godin_word_embedding(path):
+    print("Loading Goding model.")
+    return godin_embedding.Word2Vec.load_word2vec_format(path, binary=True)
+
+
+# In[190]:
+
+
+def load_google_word2vec(file_name):
+    print("Loading google news word2vec")
+    return KeyedVectors.load_word2vec_format(file_name, binary=True)
+
+
+# In[191]:
+
+
+def get_word_embedding_matrix(model,dim):
+    #dim = 300 for google word2vec
+    #dim = 400 for godin
+    #dim = 100 for fast text
+    embedding_matrix = np.zeros((vocab_size,dim))
+    for word, i in tokenizer.word_index.items():
+        try:
+            embedding_vector = model[word]
+        except KeyError:
+            embedding_vector = None
+        if embedding_vector is not None:
+            embedding_matrix[i]=embedding_vector
+    return embedding_matrix
+
+
+# In[192]:
+
+
+tokenizer = create_tokenizer(trainX)
+vocab_size = len(tokenizer.word_index) + 1
+print('Max document length: %d' % max_len)
+print('Vocabulary size: %d' % vocab_size)
+trainX = encode_text(tokenizer, trainX, max_len)
+testX = encode_text(tokenizer, testX, max_len)
+trainY = to_categorical(trainY,num_classes=number_of_classes)
+
+
+# In[144]:
+
+
+# godin_model = load_godin_word_embedding("../word_embeddings/word2vec_twitter_model.bin")
+word2vec_model= load_google_word2vec('../word_embeddings/GoogleNews-vectors-negative300.bin')
+
+
+# In[193]:
+
+
+embedding_matrix_word2vec = get_word_embedding_matrix(word2vec_model,300)
+# embedding_matrix_godin = get_word_embedding_matrix(godin_model,400)
+
+
+# ## base models
+
+# In[194]:
+
+
+cnn_parameter = {'batch': 8,
+                'dropout': 0.6,
+                'em': 'embedding_matrix_word2vec',
+                'em_trainable_flag': True,
+                'epoch': 10,
+                'filter_size': 6,
+                'learning_rate': 0.0001,
+                'n_dense': 200,
+                'n_filters': 100}
+
+lstm_parameter={'batch': 64,
+                    'dropout': 0.6,
+                    'em': 'embedding_matrix_word2vec',
+                    'em_trainable_flag': False,
+                    'epoch': 20,
+                    'learning_rate': 0.0034157107277860235,
+                    'units_out': 128}
+
+cnn_lstm_parameter={'batch': 8,
+                    'conv_dropout': 0.5,
+                    'em': 'embedding_matrix_word2vec',
+                    'em_trainable_flag': False,
+                    'epoch': 10,
+                    'filter_size': 1,
+                    'learning_rate': 0.001,
+                    'lstm_dropout': 0.4,
+                    'n_filters': 100,
+                    'units_out': 64}
+
+cnn_bi_lstm_parameter={'batch': 8,
+                    'conv_dropout': 0.5,
+                    'em': 'embedding_matrix_word2vec',
+                    'em_trainable_flag': False,
+                    'epoch': 5,
+                    'filter_size': 1,
+                    'learning_rate': 0.001,
+                    'lstm_dropout': 0.2,
+                    'n_filters': 100,
+                    'units_out': 64}
+
+
+# In[195]:
+
+
+# cnn
+# 0.4710
+def init_cnn():
+    return cnn(length=max_len,
+               vocab_size=vocab_size,
+               learning_rate=cnn_parameter['learning_rate'],
+               n_dense=cnn_parameter['n_dense'],
+               dropout=cnn_parameter['dropout'],
+               n_filters=cnn_parameter['n_filters'],
+               filter_size=cnn_parameter['filter_size'],
+               em=eval(cnn_parameter['em']),
+               number_of_classes=number_of_classes,
+               em_trainable_flag=cnn_parameter['em_trainable_flag'])
+
+
+# In[196]:
+
+
+#  lstm
+# 0.4701
+def init_lstm():
+    return lstm(length=max_len,
+                vocab_size=vocab_size,
+                learning_rate=lstm_parameter['learning_rate'],
+                dropout=lstm_parameter['dropout'],
+                units_out=lstm_parameter['units_out'],
+                em=eval(lstm_parameter['em']),
+                number_of_classes=number_of_classes,
+                em_trainable_flag=lstm_parameter['em_trainable_flag'])
+
+
+# In[197]:
+
+
+# bi_lstm
+# 0.4529
+def init_bi_lstm():
+    bi_lstm_parameter={'batch':8,
+                 'dropout': 0.6,
+                 'em': 'embedding_matrix_word2vec',
+                 'em_trainable_flag': False,
+                 'epoch': 5,
+                 'learning_rate': 0.0001,
+                 'units_out': 256}
+    return bi_lstm(length=max_len,
+                vocab_size=vocab_size,
+                learning_rate=bi_lstm_parameter['learning_rate'],
+                dropout=bi_lstm_parameter['dropout'],
+                units_out=bi_lstm_parameter['units_out'],
+                em=eval(bi_lstm_parameter['em']),
+                number_of_classes=number_of_classes,
+                em_trainable_flag=bi_lstm_parameter['em_trainable_flag'])
+
+
+# In[198]:
+
+
+# cnn_lstm
+# 0.4179
+def init_cnn_lstm():
+    return cnn_lstm(length=max_len,
+                    vocab_size=vocab_size,
+                    learning_rate=cnn_lstm_parameter['learning_rate'],
+                    n_filters=cnn_lstm_parameter['n_filters'],
+                    filter_size=cnn_lstm_parameter['filter_size'],
+                    em=eval(cnn_lstm_parameter['em']),
+                    number_of_classes=number_of_classes,
+                    em_trainable_flag=cnn_lstm_parameter['em_trainable_flag'],
+                    conv_dropout=cnn_lstm_parameter['conv_dropout'],
+                    l_or_g_dropout=cnn_lstm_parameter['lstm_dropout'],
+                    units_out=cnn_lstm_parameter['units_out'])
+
+
+# In[199]:
+
+
+# cnn_bi_lstm
+# 0.4705
+def init_cnn_bi_lstm():
+    return cnn_bi_lstm(length=max_len,
+                    vocab_size=vocab_size,
+                    learning_rate=cnn_bi_lstm_parameter['learning_rate'],
+                    n_filters=cnn_bi_lstm_parameter['n_filters'],
+                    filter_size=cnn_bi_lstm_parameter['filter_size'],
+                    em=eval(cnn_bi_lstm_parameter['em']),
+                    number_of_classes=number_of_classes,
+                    em_trainable_flag=cnn_bi_lstm_parameter['em_trainable_flag'],
+                    conv_dropout=cnn_bi_lstm_parameter['conv_dropout'],
+                    l_or_g_dropout=cnn_bi_lstm_parameter['lstm_dropout'],
+                    units_out=cnn_bi_lstm_parameter['units_out'])
+
+
+# In[200]:
+
+
+def get_pred_of_model(m,epoch,batch):
+    history = m.fit(trainX,trainY,epochs=epoch,batch_size=batch)
+    pred = m.predict(testX)    
+    pred_class = np.argmax(pred,axis=1)
+    pred_class=pred_class.astype(int)
+    acc = accuracy_score(testY,pred_class)
+    print(acc)
+#     print(pred)
+    return acc,pred_class,pred
+
+
+# In[201]:
+
+
+pred_prob_base = np.zeros((len(testX),number_of_classes,5))
+pred_class_base = np.zeros((len(testX),5),dtype=np.int32)
+acc_results={}
+
+
+# In[202]:
+
+
+acc_results['cnn'],pred_class_base[:,0],pred_prob_base[:,:,0] = get_pred_of_model(init_cnn(),cnn_parameter['epoch'],cnn_parameter['batch'])
+
+
+# In[203]:
+
+
+acc_results['lstm'],pred_class_base[:,1],pred_prob_base[:,:,1] = get_pred_of_model(init_lstm(),lstm_parameter['epoch'],lstm_parameter['batch'])
+
+
+# In[204]:
+
+
+acc_results['bi_lstm'],pred_class_base[:,2],pred_prob_base[:,:,2] = get_pred_of_model(init_bi_lstm(),bi_lstm_parameter['epoch'],bi_lstm_parameter['batch'])
+
+
+# In[205]:
+
+
+acc_results['cnn_lstm'],pred_class_base[:,3],pred_prob_base[:,:,3] = get_pred_of_model(init_cnn_lstm(),cnn_lstm_parameter['epoch'],cnn_lstm_parameter['batch'])
+
+
+# In[206]:
+
+
+acc_results['cnn_bi_lstm'],pred_class_base[:,4],pred_prob_base[:,:,4] = get_pred_of_model(init_cnn_bi_lstm(),cnn_bi_lstm_parameter['epoch'],cnn_bi_lstm_parameter['batch'])
+
+
+# In[207]:
+
+
+acc_results
+
+
+# In[210]:
+
+
+pred_class_base[:10]
+
+
+# In[212]:
+
+
+pred_prob_base[:2]
+
+
+# ## prediction corelation
+
+# In[214]:
+
+
+pred_df = pd.DataFrame(pred_class_base)
+pred_df.columns = ["cnn","lstm","bi_lstm","cnn_lstm","cnn_bi_lstm"]
+
+
+# In[216]:
+
+
+pred_df.head()
+
+
+# In[240]:
+
+
+corrmat(pred_df.corr(), inflate=False,show=False)
+plt.savefig('results/corr_matrix.png', bbox_inches='tight')
+
+
+# ## average
+
+# In[218]:
+
+
+avg_pred_prob = pred_prob_base.mean(axis=2)
+
+
+# In[219]:
+
+
+avg_pred_class = np.argmax(avg_pred_prob,axis=1)
+avg_pred_class=avg_pred_class.astype(int)
+
+
+# In[222]:
+
+
+acc = accuracy_score(testY,avg_pred_class)
+acc
+
+
+# In[223]:
+
+
+acc_results['average'] = acc
+
+
+# In[224]:
+
+
+acc_results
+
+
+# ## Majority
+
+# In[225]:
+
+
+majority_pred_class = [int(np.argmax(np.bincount(x))) for x in pred_class_base]
+
+
+# In[227]:
+
+
+acc = accuracy_score(testY,majority_pred_class)
+acc
+
+
+# In[228]:
+
+
+acc_results['majority'] = acc
+
+
+# In[229]:
+
+
+acc_results
+
+
+# ## saving results
+
+# In[230]:
+
+
+with open('results/ens_result.json','w') as fout:
+    json.dump(acc_results,fout,indent=4)
+
